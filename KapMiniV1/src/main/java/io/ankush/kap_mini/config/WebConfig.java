b4875d8e8b77cfccd48b1896374d4e03d129c69b
Ankush <ankush.wairagade@kapturecrm.com>
2025-01-20 20:51:21 +0530

    v1.. disable

--- KapMiniV1/src/main/java/io/ankush/kap_mini/config/WebConfig.java
+++ KapMiniV1/src/main/java/io/ankush/kap_mini/config/WebConfig.java
@@ -1,22 +1,14 @@
 package io.ankush.kap_mini.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost")
-                        .allowedOrigins("http://http://34.170.27.14:8080")// Replace with your React app's domain
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(""); // No origins allowed
     }
 }
